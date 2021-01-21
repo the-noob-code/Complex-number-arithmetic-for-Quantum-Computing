19cb88af32cb8a3f835e27210dbd8d572360eb14
the-noob-code <75434631+the-noob-code@users.noreply.github.com>
2021-01-21 13:06:56 +0530

    Update hermitian.py

--- hermitian.py
+++ hermitian.py
@@ -1,23 +1,23 @@
-from vector import *
 import numpy as np
+from vector import *
 
 def hermitian(matrix):
-	if matrix.all()==matrix.H.all():
+	if matrix.all() == matrix.H.all():
 		print("Hermitian Matrix")
 	else:
 		print("Not Hermitian Matrix")	
 
 def unitary(matrix):
-	mat=np.matmul(matrix,matrix.H)
-	if mat.all()==np.eye(matrix.ndim).all():
+	mat = np.matmul(matrix, matrix.H)
+	if mat.all() == np.eye(matrix.ndim).all():
 		print("Unitary Matrix")
 	else:
 		print("Not Unitary Matrix")
 
-def tensor(matrix1,matrix2):
-	n,m=matrix1.shape
-	li=list()
+def tensor(matrix1, matrix2):
+	n,m = matrix1.shape
+	li = list()
 	for i in range(n):
 		for j in range(m):
-			li.append(srm_matrix(matrix2,matrix1[i,j]))
+			li.append(srm_matrix(matrix2, matrix1[i,j]))
 	return np.array(li)		
